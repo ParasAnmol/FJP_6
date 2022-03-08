f8779bf1584c0bf7e5f148975a10370adfdef65d
ParasAnmol <parasanmol16@gmail.com>
2022-03-08 15:32:28 -0500

    print and replace a array

--- module2/array.js
+++ module2/array.js
@@ -7,3 +7,9 @@ console.log(arr);
 
 let elarr = [5 , 6 , 7 , 'hello string', false , 'a'];
 console.log(elarr);
+console.log(elarr[3]);
+
+// replace a element in array 
+
+elarr[5] = 'make js on finger tips';
+console.log(elarr[5]);
