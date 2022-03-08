f5806c7f911982f849a06795cc0dbe5bf53b13e0
ParasAnmol <parasanmol16@gmail.com>
2022-03-08 14:15:59 -0500

    function with and without parameter

--- module2/function.js
+++ module2/function.js
@@ -3,3 +3,12 @@ function hello(){
     console.log("you gonna nail js");
 }
 hello();
+
+// function with parameters
+
+function sum(a,b){
+    let add = a + b ;
+    console.log("add of two num is " + add);
+}
+
+sum(3,4);
