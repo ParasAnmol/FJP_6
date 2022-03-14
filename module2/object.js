f1f0993c2b85c7a7439fce692711522d7ae660de
ParasAnmol <parasanmol16@gmail.com>
2022-03-14 08:29:07 -0400

    intro to object

---
+++ module2/object.js
@@ -0,0 +1,10 @@
+ let obj = {}; //empty object
+ console.log(obj);
+
+ let batman ={
+     Name : "Bruce",
+     age : 32,
+     phoneno : 0000000000,
+     Lastname : 'Wayne'
+ };
+ console.log(batman);
