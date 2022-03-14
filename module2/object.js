8c664427b3b96e554be058a40968fc8e4891987e
ParasAnmol <parasanmol16@gmail.com>
2022-03-14 08:58:20 -0400

    Using different keys in object

--- module2/object.js
+++ module2/object.js
@@ -4,7 +4,23 @@
  let batman ={
      Name : "Bruce",
      age : 32,
-     phoneno : 0000000000,
+     phoneno : 7865341209,
      Lastname : 'Wayne'
  };
  console.log(batman);
+
+ let Vigilante ={
+    Name : "Bruce",
+    age : 32,
+    phoneno : 7865341209,
+    Lastname : 'Wayne',
+    friends : ['robin','Selena','jim gorden','alfred'],
+    address : {
+        City : "Gotham",
+        Address : "Wayne tower"
+    },
+    Say : function(){
+        console.log("I am vengence");
+    }
+}; 
+console.log(Vigilante);
