7d4e4f46996ce15c627d04dab5aecee29b2078ec
ParasAnmol <parasanmol16@gmail.com>
2022-03-14 09:05:10 -0400

    method to access different keys in object

--- module2/object.js
+++ module2/object.js
@@ -24,3 +24,16 @@
     }
 }; 
 console.log(Vigilante);
+
+// Print name 
+console.log(Vigilante.Name);
+//print array
+console.log(Vigilante.friends);
+//print a selected array number
+console.log(Vigilante.friends[3]);
+//print city form address
+console.log(Vigilante.address.City);
+//print function if there is no return
+Vigilante.Say();
+//print function if there is something to return
+console.log(Vigilante.Say());
