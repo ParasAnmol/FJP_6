03fa5bb9bd7f75f547aeb566b36492487e3b613c
ParasAnmol <parasanmol16@gmail.com>
2022-03-15 12:51:29 -0400

    q3 complete

--- module2/intronode/questions/q3.js
+++ module2/intronode/questions/q3.js
@@ -1,1 +1,10 @@
 //copy index.html file from module1 to a new folder inside module2 having name html.
+let fs = require('fs');
+
+let path = require('path');
+// if(!fs.existsSync("html")){
+//     fs.mkdirSync("/home/wamir/FJP_6/module2/html");
+// }
+let spath = "/home/wamir/FJP_6/module1/minor project/index.html";
+let dpath = "/home/wamir/FJP_6/module2/html/index.html";
+fs.copyFileSync(spath,dpath);
