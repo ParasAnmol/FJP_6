947794567beeb0cfc314f4ec74f5ac28be0d81de
ParasAnmol <parasanmol16@gmail.com>
2022-03-15 12:10:53 -0400

    copy a file

--- module2/intronode/fs.js
+++ module2/intronode/fs.js
@@ -2,27 +2,32 @@ let fs = require('fs');
 console.log(fs);
 let path = require('path');
 
-let fpath = path.join(__dirname,"file.txt");
-console.log(fpath);
-// Create file /
-fs.writeFileSync(fpath, "You nailed it");
-fs.writeFileSync(fpath, "You Nailed it long time ago");
-// Read file 
-let content =fs.readFileSync(fpath,'UTF-8');
-console.log(content);
-// update file content 
-fs.appendFileSync(fpath, " New thing");
-console.log(fs.readFileSync(fpath,'UTF-8'));
-// delete
-fs.unlinkSync(fpath);
+// let fpath = path.join(__dirname,"file.txt");
+// console.log(fpath);
+// // Create file /
+// fs.writeFileSync(fpath, "You nailed it");
+// fs.writeFileSync(fpath, "You Nailed it long time ago");
+// // Read file 
+// let content =fs.readFileSync(fpath,'UTF-8');
+// console.log(content);
+// // update file content 
+// fs.appendFileSync(fpath, " New thing");
+// console.log(fs.readFileSync(fpath,'UTF-8'));
+// // delete
+// fs.unlinkSync(fpath);
 
-// New folder making 
-if(!fs.existsSync("NewDir")){
-    fs.mkdirSync("NewDir")
-}
-//reada dir
+// // New folder making 
+// if(!fs.existsSync("NewDir")){
+//     fs.mkdirSync("NewDir")
+// }
+// //reada dir
 
-console.log(fs.readdirSync("/home/wamir/FJP_6/module1"));
-// remove dir 
+// console.log(fs.readdirSync("/home/wamir/FJP_6/module1"));
+// // remove dir 
 
-fs.rmdirSync("NewDir");
+// fs.rmdirSync("NewDir");
+let spath = path.join(__dirname,"abc.txt");
+let dpath = path.join(__dirname,"questions","abc.txt");
+console.log(spath);
+console.log(dpath);
+fs.copyFileSync(spath,dpath);
