3efe8b1dcdbf43ee1896503b9e9d132fc2462799
ParasAnmol <parasanmol16@gmail.com>
2022-03-15 17:45:18 -0400

    4th question Intialize

--- module2/intronode/questions/q4.js
+++ module2/intronode/questions/q4.js
@@ -1,1 +1,5 @@
 // read content of unorganised folder and make  an array which has extension name of each file
+let fs = require('fs');
+let path = require('path');
+
+console.log(fs.readdirSync("/home/wamir/FJP_6/module2/intronode/unorganised"));
