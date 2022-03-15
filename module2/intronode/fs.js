4704f22a84aec9f525eda5f88f9959deb645063d
ParasAnmol <parasanmol16@gmail.com>
2022-03-15 09:46:57 -0400

    Read a file with fs

--- module2/intronode/fs.js
+++ module2/intronode/fs.js
@@ -4,5 +4,9 @@ let path = require('path');
 
 let fpath = path.join(__dirname,"file.txt");
 console.log(fpath);
+// Create file /
 fs.writeFileSync(fpath, "You nailed it");
 fs.writeFileSync(fpath, "You Nailed it long time ago");
+// Read file 
+let content =fs.readFileSync(fpath,'UTF-8');
+console.log(content);
