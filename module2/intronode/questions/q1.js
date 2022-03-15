39002b01f2f583678010eb9dbafd77061f469bef
ParasAnmol <parasanmol16@gmail.com>
2022-03-15 12:25:31 -0400

    q1 complete

--- module2/intronode/questions/q1.js
+++ module2/intronode/questions/q1.js
@@ -1,2 +1,10 @@
 //make a folder inside same directory of this file and in that folder make a text file with content
 //new file has been made
+let fs = require('fs');
+
+let path = require('path');
+if(!fs.existsSync("qone")){
+    fs.mkdirSync("qone")
+}
+let fpath= path.join(__dirname,"qone","q1.txt");
+fs.writeFileSync(fpath,"You gonna nail it");
