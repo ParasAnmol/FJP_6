ec806ea8862c23d6c3c2a78123347aeb89ad9438
ParasAnmol <parasanmol16@gmail.com>
2022-03-19 12:13:14 -0400

    q4 completed

--- module2/intronode/questions/q4.js
+++ module2/intronode/questions/q4.js
@@ -1,5 +1,14 @@
 // read content of unorganised folder and make  an array which has extension name of each file
 let fs = require('fs');
 let path = require('path');
+let fpath = path.join(__dirname,"..","unorganised");
 
-console.log(fs.readdirSync("/home/wamir/FJP_6/module2/intronode/unorganised"));
+let redaf = fs.readdirSync(fpath);
+console.log(redaf);
+console.log(redaf.length);
+let extarr = [];
+for(let i = 0; i <= redaf.length-1; i++){
+    let ext = path.extname(redaf[i]);
+    extarr.push(ext);
+}
+console.log(extarr);
