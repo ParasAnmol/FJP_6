68afbf041b663c077db55127d0bf5dfb056ba59d
ParasAnmol <parasanmol16@gmail.com>
2022-03-19 12:25:17 -0400

    q5 completed

--- module2/intronode/questions/q5.js
+++ module2/intronode/questions/q5.js
@@ -1,1 +1,12 @@
 //move a file
+let fs = require('fs');
+let path = require('path');
+
+let source = path.join(__dirname,"abc.txt");
+console.log(source);
+let destin = path.join(__dirname,"..","unorganised","abc.txt");
+console.log(destin);
+
+fs.copyFileSync(source,destin);
+
+fs.unlinkSync(source);
