2f4800034543fdbff2d210299edad927481622d6
ParasAnmol <parasanmol16@gmail.com>
2022-03-29 11:04:44 -0400

    Succesfully printing names of team

--- module2/webScarpping/request.js
+++ module2/webScarpping/request.js
@@ -1,10 +1,17 @@
 const request = require('request');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
-request('http://www.google.com',cb); 
+request('https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/chennai-super-kings-vs-kolkata-knight-riders-1st-match-1304047/full-scorecard',cb); 
 
-function cb(error, response, body) {
+function cb(error, response, html) {
     if (error)  
         console.error('error:', error); // Print the error if one occurred
-    else
-        console.log('body:', body); // Print the HTML for the Google homepage.
+    else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let teamsname = document.querySelectorAll(".name-link .name ");
+        console.log(teamsname[0].textContent);
+        console.log(teamsname[1].textContent);
+    }
 }
